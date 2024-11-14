e76665b676b85fe53282e36f0f205be44e61a66c
ParraJesus <jgparra@unicauca.edu.co>
2024-11-14 17:06:31 -0500

    prueba cliente-servidor

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function App(){
+  const [backendData, setBakendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      Response => Response.json()
+    ).then(
+      data => {
+        setBakendData(data)
+      }
+    )
+  })
+
+  return(
+    <div>
+      {(typeof backendData.users === 'undefined') ? (<p>Loading...</p>) : 
+      (
+        backendData.users.map((user, i) => (<p key={i}>{user}</p>))
+      )
+      }
+    </div>
+  )
+}
+
+export default App
